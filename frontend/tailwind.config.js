396324db79215a19fa3d226c46a9a2afcb50af83
mohammedaitgaba <medgaba237@gmail.com>
2022-05-25 16:14:54 +0100

    updating tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  purge: [],
   content: [
-    "./node_modules/flowbite/**/*.js"
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-],
-
+  plugins: [],
 }
